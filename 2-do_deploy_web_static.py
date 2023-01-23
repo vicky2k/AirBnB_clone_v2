98e73909f1f5a26ccb930dc4ab7936d145e39638
WavuaW <wavuamuroka@gmail.com>
2023-01-23 18:46:26 +0300

    corrections to task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,9 +6,9 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['100.26.217.114', '54.144.129.83']
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
+env.key_filename = '~/.ssh/New'
 
 
 def do_deploy(archive_path):
